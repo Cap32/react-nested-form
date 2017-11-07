39f2c0f3f4730f8fdcf7e3367212ba8f38c4b7f9
3went2 <webb.png@gmail.com>
2017-11-07 17:26:21 +0800

    fix dataType=date bug

--- src/Mixins.js
+++ src/Mixins.js
@@ -1,12 +1,14 @@
 
-import { createFormatDataTypeFunc } from './DataTypes';
+import { createFormatDataTypeFunc as createFormator } from './DataTypes';
 import { createFormatEmptyFunc } from './utils';
 
 const filterFn = function filterFn(Component, methodName, propName) {
 	Component.prototype[methodName] = function (val) {
 		const { props, props: { dataType, formatEmptyValue } } = this;
 		const ioFilters = props[propName] || [];
-		const headFilter = dataType ? [createFormatDataTypeFunc(dataType)] : [];
+		const isInputDate = propName === 'inputFilter' && dataType === 'date';
+		const finalDataType = isInputDate ? 'dateTime' : dataType;
+		const headFilter = finalDataType ? [createFormator(finalDataType)] : [];
 		const lastFilter = createFormatEmptyFunc(formatEmptyValue);
 		const filters = headFilter.concat(ioFilters).concat(lastFilter);
 		return filters.reduce((res, fn) => fn(res, props), val);
